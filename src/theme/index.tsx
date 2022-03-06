45aad403717550c1feed90074f9e63e51f525f02
Ruy Freire <ruy.slv@gmail.com>
2022-03-06 19:19:28 -0300

    mudança de cores

--- src/theme/index.tsx
+++ src/theme/index.tsx
@@ -8,61 +8,106 @@ import {
 export const theme = {
   colors: {
     primary: {
+      /** base: '#2059A4' */
       base: '#2059A4',
-      light: '#5c85d6',
-      dark: '#003175'
+      /** light: '#2567be' */
+      light: '#2567be',
+      /** dark: '#1c5093' */
+      dark: '#1c5093'
     },
     secondary: {
-      base: '#5ECCD1',
-      light: '#94ffff',
-      dark: '#1d9ba0'
+      /** base: '#47A4A9' */
+      base: '#47A4A9',
+      /** light: '#53b2b7' */
+      light: '#53b2b7',
+      /** dark: '#3f9398' */
+      dark: '#3f9398'
     },
     danger: {
+      /** base: '#D8655F' */
       base: '#D8655F',
-      light: '#ff958c',
-      dark: '#a23635'
+      /** light: '#db746f' */
+      light: '#db746f',
+      /** dark: '#d14c45' */
+      dark: '#d14c45'
     },
     warning: {
+      /** base: '#F1DC73' */
       base: '#F1DC73',
-      light: '#ffffa4',
-      dark: '#bcab44'
+      /** light: '#f2df80' */
+      light: '#f2df80',
+      /** dark: '#edd352' */
+      dark: '#edd352'
     },
     success: {
+      /** base: '#64D870' */
       base: '#64D870',
-      light: '#99ffa0',
-      dark: '#29a642'
+      /** light: '#73db7e' */
+      light: '#73db7e',
+      /** dark: '#4ad158' */
+      dark: '#4ad158'
     },
-    grayLight: '#C4C4C4',
-    grayMedium: '#565656',
-    grayDark: '#191919',
+    gray: {
+      /** base: '#838383' */
+      base: '#838383',
+      /** light: '#c7c7c7' */
+      light: '#c7c7c7',
+      /** dark: '#565656' */
+      dark: '#565656'
+    },
+    /** text: '#2d2d2d' */
+    text: '#2d2d2d',
+    /** black: '#000000' */
     black: '#000000',
+    /** offWhite: '#fafafa' */
+    offWhite: '#fafafa',
+    /** white: '#FFFFFF' */
     white: '#FFFFFF'
   },
   sizes: {
+    /** xxs: '1px' */
     xxs: '1rem',
+    /** xs: '12px' */
     xs: '1.2rem',
+    /** md: '16px' */
     md: '1.6rem',
+    /** lg: '2px' */
     lg: '2rem',
+    /** xl: '24px' */
     xl: '2.4rem',
+    /** xxl: '28px' */
     xxl: '2.8rem'
   },
   fontWeights: {
-    light: 300,
-    normal: 400,
-    medium: 500,
+    /** light: 100 */
+    light: 100,
+    /** normal: 300 */
+    normal: 300,
+    /** medium: 400 */
+    medium: 400,
+    /** bold: 700 */
     bold: 700
   },
   spacing: {
+    /** xs: '8px' */
     xs: '8px',
+    /** md: '16px' */
     md: '16px',
+    /** lg: '24px' */
     lg: '24px',
+    /** xl: '32px' */
     xl: '32px'
   },
   radius: {
+    /** xs: '2px' */
     xs: '2px',
+    /** md: '4px' */
     md: '4px',
-    lg: '8px',
-    full: '100px'
+    /** lg: '8px' */
+    lg: '8px'
+  },
+  fontFamily: {
+    base: '"Lato", Roboto, sans-serif'
   }
 }
 
@@ -78,11 +123,11 @@ export const GlobalStyle = createGlobalStyle`
   }
 
   html {
-    font-size: 62.5%;
+    font-size: 10px;
   }
 
   body {
-    font-family: 'Roboto', sans-serif;
+    font-family: ${theme.fontFamily.base};
     font-size: ${theme.sizes.md};
   }
 `
