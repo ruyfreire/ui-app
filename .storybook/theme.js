dc834bd0120ec8835a7ea37196183a0c12930b3a
Ruy Freire <ruy.slv@gmail.com>
2022-03-06 19:20:02 -0300

    configs do storybook

---
+++ .storybook/theme.js
@@ -0,0 +1,8 @@
+import { create } from '@storybook/theming'
+
+export default create({
+  colorPrimary: '#2059A4',
+  colorSecondary: '#5ECCD1',
+  brandTitle: 'UI APP',
+  brandUrl: 'https://github.com/ruyfreire/ui-app'
+})
