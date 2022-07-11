c2718b2f50de04a800d8237f281acfbfa3c36045
Ruy Freire <ruy.slv@gmail.com>
2022-07-11 20:10:13 -0300

    (IconButton) Novo componente

---
+++ src/components/IconButton/styles.ts
@@ -0,0 +1,77 @@
+import styled, { css, DefaultTheme, keyframes } from 'styled-components'
+import { IconButtonProps } from '.'
+
+const rotate = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+
+  to {
+    transform: rotate(360deg);
+  }
+`
+
+const sizes = {
+  small: (theme: DefaultTheme) => css`
+    width: 24px;
+    height: 24px;
+    padding: 6px;
+
+    box-shadow: inset 0 0 0 2px transparent;
+
+    :hover:not(:disabled),
+    :focus:not(:disabled) {
+      box-shadow: inset 0 0 0 2px ${theme.colors.white3};
+    }
+  `,
+  medium: (theme: DefaultTheme) => css`
+    width: 48px;
+    height: 48px;
+    padding: 12px;
+
+    box-shadow: inset 0 0 0 4px transparent;
+
+    :hover:not(:disabled),
+    :focus:not(:disabled) {
+      box-shadow: inset 0 0 0 4px ${theme.colors.white3};
+    }
+  `
+}
+
+export const Wrapper = styled.button<IconButtonProps>`
+  ${({ theme, size }) => css`
+    color: ${theme.colors.white};
+    font-size: 0;
+
+    display: inline-flex;
+    align-items: center;
+    justify-content: center;
+    cursor: pointer;
+    border: 0;
+    border-radius: 50%;
+    transition: ${theme.transitions.all};
+    position: relative;
+    background-color: ${theme.colors.white3};
+
+    :disabled {
+      cursor: not-allowed;
+      background-color: ${theme.colors.disabled};
+      color: ${theme.colors.white2};
+    }
+
+    ${size && sizes[size](theme)}
+  `}
+`
+
+export const IconLoader = styled.i`
+  ${({ theme }) => css`
+    position: relative;
+    transition: ${theme.transitions.all};
+
+    svg {
+      animation: ${rotate} 1s linear infinite;
+      width: 100%;
+      height: 100%;
+    }
+  `}
+`
