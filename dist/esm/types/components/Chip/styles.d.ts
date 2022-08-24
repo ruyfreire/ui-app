b66d7a181d50373247a41e509eb16356f12bc936
Ruy Freire <ruy.slv@gmail.com>
2022-08-24 20:45:24 -0300

    remover dist

--- dist/esm/types/components/Chip/styles.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="react" />
-import { DefaultTheme } from 'styled-components';
-export declare const Chip: import("styled-components").StyledComponent<"div", DefaultTheme, import("react").HTMLAttributes<HTMLDivElement> & {
-    size?: "small" | "medium" | undefined;
-    icon?: import("react").ReactNode;
-    disabled?: boolean | undefined;
-    selected?: boolean | undefined;
-}, never>;
-export declare const Icon: import("styled-components").StyledComponent<"i", DefaultTheme, {}, never>;
